33db6184e390acd6a8eab448eaa93b1a640720df
John Pham <johnpham7496@gmail.com>
2020-08-18 10:03:15 -0700

    added pipfile and pip.lock files, updated app.py file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+my_app = Flask(__name__)
+
+
+@my_app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    my_app.run(host='localhost', port=5000, debug=True)
+
+
